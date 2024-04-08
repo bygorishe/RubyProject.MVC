8e0edd8c452c68291004392913ee18b23e757e3b
bygor <angrydogeee@gmail.com>
2024-04-08 20:13:38 +0300

    [laba-5] создал новый контроллер

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :themes
   resources :images
   resources :users
+
+  # work
+  match 'work',             to: 'work#index',                  via: 'get'
+  match 'choose_theme',     to: 'work#choose_theme',      via: :get
+  match 'display_theme',    to: 'work#display_theme',      via: :post
+
   get 'main/index'
   get 'main/help'
   get 'main/contacts'
@@ -10,6 +16,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'main#index'
+  root 'work#index'
 
 end
