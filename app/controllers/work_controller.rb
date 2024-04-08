e0c8a90f6c53d16a70603c3501fdd564da6f9561
bygor <angrydogeee@gmail.com>
2024-04-08 21:21:45 +0300

    [laba-5] начал делать

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,5 +1,12 @@
 class WorkController < ApplicationController
   def index
+    @images_count = Image.all.count
+    # @selected_theme = "Select theme to leave your answer"
+    @selected_theme = t(".def_select_theme")
+    @selected_image_name = 'радуга'
+    @values_qty = Value.all.count
+    @current_locale = I18n.locale
+    session[:selected_theme_id] = @selected_theme # to display nothing
   end
 
   def choose_theme
